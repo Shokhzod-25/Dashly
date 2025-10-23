76fa772c0264048206f8b4f52cae788a76f8bf4d
Shokhzod <shahzodergashev2555@icloud.com>
2025-10-24 00:58:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from src import create_app
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:create_app", reload=True, factory=True)
+    uvicorn.run("main:create_app", reload=True, host="0.0.0.0", port=4000, factory=True)
