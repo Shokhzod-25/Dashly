85fafca34e24c4444c74ccf94925994bbf79b61d
Shokhzod <shahzodergashev2555@icloud.com>
2025-10-24 12:03:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import uvicorn
 from src import create_app
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PORT = os.getenv("PORT")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:create_app", reload=True, host="0.0.0.0", port=4000, factory=True)
+    uvicorn.run("main:create_app", reload=True, host="0.0.0.0", port=int(PORT), factory=True)
