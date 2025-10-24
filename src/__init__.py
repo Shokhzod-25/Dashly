bb50b4e0634d4062057815656609605782095beb
Shokhzod <shahzodergashev2555@icloud.com>
2025-10-24 12:01:59 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,13 @@ from src.api import router
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Dashly API", docs_url="/")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(router)
 
     return app
